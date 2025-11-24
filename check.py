5306d9e75b69f1cb9509d9e04c86c07bf21b52dc
Amin Abdessalem <amineabdessalem140@gmail.com>
2025-11-24 20:33:38 +0100

    check availability of the local GPU

--- check.py
+++ check.py
@@ -1,6 +1,6 @@
 import torch
-print("CUDA available:", torch.cuda.is_available())
+print("CUDA available (GPU available):", torch.cuda.is_available())
 print("CUDA device count:", torch.cuda.device_count())
 if torch.cuda.is_available():
-    print("Current CUDA device:", torch.cuda.current_device())
-    print("Device name:", torch.cuda.get_device_name(torch.cuda.current_device()))
+    print("Current CUDA device: (GPU number)", torch.cuda.current_device())
+    print("Device name: (GPU name)", torch.cuda.get_device_name(torch.cuda.current_device()))
